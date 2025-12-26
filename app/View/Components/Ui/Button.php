80090774e8dbd48e6eaeebd675922b81c800e2ea
uzunvictor <v.lungu@webdelo.com>
2025-12-26 08:59:52 +0200

    added types for button component

--- app/View/Components/Ui/Button.php
+++ app/View/Components/Ui/Button.php
@@ -8,10 +8,15 @@ use Illuminate\View\Component;
 
 class Button extends Component
 {
+    public array $types = [
+        'default' => 'bg-white text-black border-[#E7E7EC] active:bg-gray-200 hover:bg-gray-200',
+        'danger' => 'bg-[#E92B43] border-[#E92B43] text-white font-medium active:bg-red-800 hover:bg-red-600',
+    ];
+
     /**
      * Create a new component instance.
      */
-    public function __construct(public string $message)
+    public function __construct(public string $message, public string $type = 'default')
     {
         //
     }
--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['class' => 'px-4 py-2 bg-[#E92B43] rounded-2xl text-white text-sm/4 font-medium cursor-pointer active:bg-red-800 hover:bg-red-600']) }}>
+<button {{ $attributes->merge(['class' => 'px-4 py-2 text-sm/4 border-1 rounded-2xl cursor-pointer '.$types[$type]]) }}>
     {{ $message }}
 </button>
